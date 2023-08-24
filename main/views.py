bc189d41eea7f60bef2f4bba8c398d5c48d64011
md tarik bosunia <bosuniamdtarik005@gmail.com>
2023-08-24 10:09:48 +0600

    show products in home page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from store.models import Product
 
 
 def home(request):
-    return render(request, template_name="home_page.html")
+    products = Product.objects.filter(is_available=True)
+    context = {
+        "products": products
+    }
+    return render(request, template_name="home_page.html", context=context)
+
